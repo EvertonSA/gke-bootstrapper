d5f8f2823608f7c7832903204c654f7c56fdcf17
Everton S.A <eveuca@gmail.com>
2019-08-26 12:53:19 -0300

    bundled script to separated script

--- resourses/create_kubernetes_gcp.sh
+++ resourses/create_kubernetes_gcp.sh
@@ -9,19 +9,19 @@
 # import variables in script context
 source values.sh
 
-#
+# provision network stuff
 cloud-infrastructure/00-gcloud-network.sh
 
-#
+# create service account
 cloud-infrastructure/01-gcloud-apiadmin-sa.sh
 
-#
+# create GKE cluster
 cloud-infrastructure/10-gcloud-gke.sh
 
-#
+# create storage 
 cloud-infrastructure/11-gcloud-prom-storage.sh
 
-#
+# create dns entry
 cloud-infrastructure/20-gcloud-clouddns.sh
 
 # configure cloud shell to kubernetes via clusterrolebinding
@@ -29,7 +29,14 @@ kubectl create clusterrolebinding "cluster-admin-$(whoami)" \
 --clusterrole=cluster-admin \
 --user="$(gcloud config get-value core/account)"
 
-#
+# install helm on gke
 gke-addons/install-helm-gke.sh
 
+# install cert-manager for TLS with letsencrypt
+gke-addons/install-cert-manager-gke.sh
 
+# bellow is necessary to renew istio gateway pod certificate
+kubectl -n istio-system delete pods -l istio=ingressgateway
+
+# install flagger 
+gke-addons/install-flagger-gke.sh
