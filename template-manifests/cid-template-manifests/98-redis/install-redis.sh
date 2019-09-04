10cf421a121de47e125c030eb1c3f9494cbfc0a2
Pedreiro do Amor <eveuca@gmail.com>
2019-09-04 18:41:58 -0300

    changing database and cache name

--- template-manifests/cid-template-manifests/98-redis/install-redis.sh
+++ template-manifests/cid-template-manifests/98-redis/install-redis.sh
@@ -1,2 +1,2 @@
 # echo "--------------------------------------------------INSTALL REDIT--------------------------------------------------"
-helm install --name cache-container-registry --namespace cid stable/redis
+helm install --name cache-cid --namespace cid stable/redis
