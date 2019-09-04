09c9fe8d67d69accfde8f378121b02848f5cb32c
Pedreiro do Amor <eveuca@gmail.com>
2019-09-04 18:54:39 -0300

    from pg-prod to pg-cid

--- template-manifests/cid-template-manifests/99-psql/install-psql.sh
+++ template-manifests/cid-template-manifests/99-psql/install-psql.sh
@@ -1,3 +1,3 @@
 #fist install postgress
 echo "--------------------------------------------------INSTALL POSTGRES--------------------------------------------------"
-helm install --name database-psql-cid -f ./pg-prod-values.yaml --namespace cid stable/postgresql
+helm install --name database-psql-cid -f ./pg-cid-values.yaml --namespace cid stable/postgresql
