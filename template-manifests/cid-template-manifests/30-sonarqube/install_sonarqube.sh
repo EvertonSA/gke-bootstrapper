30a97d07fd49d5783cc64182a4262ba9c4f3376d
Pedreiro do Amor <eveuca@gmail.com>
2019-09-04 19:36:45 -0300

    sonarqube configs

--- template-manifests/cid-template-manifests/30-sonarqube/install_sonarqube.sh
+++ template-manifests/cid-template-manifests/30-sonarqube/install_sonarqube.sh
@@ -1,12 +1,14 @@
 export POSTGRES_PASSWORD=$(kubectl get secret --namespace cid database-psql-cid-postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
+echo ${POSTGRES_PASSWORD}
 
-
-helm install  --namespace cid \
+helm install --name cid --namespace cid \
   --set image.tag=6.7.7-community \
   --set service.type=ClusterIP \
   --set database.type=postgresql \
   --set postgresql.enabled=false \
   --set postgresql.postgresServer=database-psql-cid-postgresql.cid.svc.cluster.local \
   --set postgresql.postgresUser=postgres \
+  --set postgresql.postgresDatabase=sonarDB \
+  --set postgresql.service.port=5432 \
   --set postgresql.postgresPassword=${POSTGRES_PASSWORD} \
   stable/sonarqube
